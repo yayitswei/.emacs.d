df8abe4c3927fb843064203ea1a6dab24fe7ee91
Wei Hsu <yayitswei@gmail.com>
2014-02-24 21:48:12 +0800

    upgrade packages

--- elpa/evil-paredit-20121117.919/evil-paredit-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "evil-paredit" "20121117.919" "Paredit support for evil keybindings [source: github]" (quote ((evil "0.0.0") (paredit "1"))))
--- elpa/linum-relative-20120905.1503/linum-relative-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "linum-relative" "20120905.1503" "display relative line number in the left margin [source: wiki]" (quote nil))
