d2f8e3a892c0955182952e9e6297c66a2bc971d6
Wei Hsu <yayitswei@gmail.com>
2020-09-01 18:52:47 -0700

    more cleanup, remove unused items

--- init.el
+++ init.el
@@ -194,23 +194,6 @@
 ;; Set color-theme
 (require 'color-theme)
 (setq color-theme-is-global t)
-;; (add-to-list 'load-path "~/.emacs.d/checkouts/emacs-color-theme-solarized")
-;; (require 'color-theme-solarized)
-;; (setq solarized-termcolors 256)
-;(color-theme-solarized-dark)
-;; (color-theme-solarized-light)
-
-(defun toggle-night-color-theme ()
-  "Switch to/from night color scheme."
-  (interactive)
-  (if (eq (frame-parameter (next-frame) 'background-mode) 'dark)
-      (color-theme-snapshot) ; restore default (light) colors
-    ;; create the snapshot if necessary
-    (when (not (commandp 'color-theme-snapshot))
-      (fset 'color-theme-snapshot (color-theme-make-snapshot)))
-    (color-theme-solarized-dark)))
-(global-set-key (kbd "<f9> n") 'toggle-night-color-theme)
-
 
 ;; (set-face-attribute 'default nil :height 120)
 ;; (set-cursor-color 'white)
